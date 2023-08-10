52fbd90b3a4b28e66a006ea9928ecc5ebb212e05
mtsheda <tmafora2@gmail.com>
2023-08-10 12:55:49 +0200

    added Vuyis work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,11 +8,9 @@ import { ProfileComponent } from './menu/profile/profile.component';
 import { UserDetailsComponent } from './message/user-details/user-details.component';
 import { PageComponent } from './page/page.component';
 import { Page2Component } from './menu/profile/page2/page2.component';
-import { MainContentRejectComponent } from './menu/main-content-reject/main-content-reject.component';
-import { MainContentComponent } from './main-content/main-content.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { TopbarComponent } from './topbar/topbar.component';
-import { ToolbarRejectComponent } from './toolbar-reject/toolbar-reject.component';
+
+
+
 
 const routes: Routes = [
   {
@@ -45,29 +43,11 @@ const routes: Routes = [
 },
 
 
-{
-  path:"main content reject", component:MainContentRejectComponent
-},
-
-{
-  path:"main content", component:MainContentComponent
-},
 
 
 
-{
-  path:"toolbar", component:ToolbarComponent
-},
 
 
-{
-  path:"topBar", component:TopbarComponent
-},
-
-
-{
-  path:"tool bar reject", component:ToolbarRejectComponent
-}
 
 
 
