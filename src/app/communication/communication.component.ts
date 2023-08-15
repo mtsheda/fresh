2002f760d353a4654a6dfc1af8666dce5d1de52b
mtsheda <tmafora2@gmail.com>
2023-08-15 11:26:26 +0200

    added links

--- src/app/communication/communication.component.ts
+++ src/app/communication/communication.component.ts
@@ -6,9 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./communication.component.scss']
 })
 export class CommunicationComponent {
-  
-  
 
-  
+  sendMessage() {
+    const input1Value = (<HTMLInputElement>document.querySelector('.input1')).value;
+    const input2Value = (<HTMLInputElement>document.querySelector('.input2')).value;
 
+    if (input1Value.trim() === '' || input2Value.trim() === '') {
+      alert('Fields cannot empty');
+      return;
+    }
+
+    
+    (<HTMLInputElement>document.querySelector('.input1')).value = '';
+    (<HTMLInputElement>document.querySelector('.input2')).value = '';
+  }
 }
+  
+  
+
+  
+
+
